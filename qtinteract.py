454ec7a22d26c787cf49e2b321f17669566ab999
Lev Maximov <lev.maximov@gmail.com>
2024-01-29 19:17:39 +0700

    minor fixes, demo

--- qtinteract.py
+++ qtinteract.py
@@ -22,15 +22,19 @@ def spin2slider(v, vmin, vmax, n):
 
 class SimpleWindow(QWidget):
     def add_param(self, name, vmin=None, vmax=None, vstep=None, v=None):
-        if any(isinstance(var, float) for var in (vmin, vmax, vstep, v)):
-            vstep = 0.1
-        else:
-            vstep = 1
+        if vstep is None:
+            if any(isinstance(var, float) for var in (vmin, vmax, vstep, v)):
+                vstep = 0.1
+            else:
+                vstep = 1
         if v is None:
-            assert vmin is not None and vmin is not None and vmin < vmax
+            assert vmin is not None and vmin is not None and vmin < vmax, name
             v = vmin + round((vmax-vmin)/vstep)//2*vstep
         elif vmin is None and vmax is None:
-            vmin, vmax = -v, v*2
+            if v == 0:
+                vmin, vmax = 0, 1
+            else:
+                vmin, vmax = -v, v*2
         assert vstep != 0
         n = round((vmax-vmin)/vstep)
         label = QLabel(text=name)
@@ -62,44 +66,82 @@ class SimpleWindow(QWidget):
             self.setWindowTitle('QtInteract')
             if len(args) == 1:
                 y = args[0]
-                x = None
+                x, style = None, None
             elif len(args) == 2:
                 x, y = args
+                style = None
+            elif len(args) == 3:
+                x, y, style = args
             else:
-                raise ValueError('There should be either one (y) or two (x, y) positional arguments, not {len(args})).')
+                raise ValueError(f'There should be either one (y), two (x, y) positional arguments, not {len(args)}.')
             if isinstance(y, (tuple, list)):
-                self.funcs = y
+                pass
             else:
-                self.funcs = [y]
-            self.nfuncs = len(self.funcs)
-            #print(self.nfuncs, 'funcs')
+                y = [y]
 
             self.layout = QVBoxLayout(self)
 
+            if isinstance(x, (tuple, list)):
+                assert len(x) == len(y), 'The number of x arrays should either be 1 or match the number of funcs'
+                assert isinstance(x[0], np.ndarray) or x[0] is None
+                self.x = x
+            elif isinstance(x, np.ndarray) or x is None:
+                self.x = [x] * len(y)
+            else:
+                raise ValueError('First argument x must either be a numpy array or a list of numpy arrays')
+
+            if isinstance(style, (tuple, list)):
+                assert len(style) == len(y), 'The number of style elements should either be 1 or match the number of funcs'
+                assert isinstance(style[0], str)
+                pass
+            elif isinstance(style, str) or style is None:
+                style = [style] * len(y)
+            else:
+                raise ValueError('Third argument style must either be a str or a list of strs')
+
             self.canvas = pg.PlotWidget()
             self.plots = []
-            for i, f in enumerate(self.funcs):
-                self.plots.append(self.canvas.plot([], [], pen='b', name=f'f{i}'))
+            self.static_plots = []
+            self.funcs = []
+            self.funcs_x = []
+            default_args = {}
+            for i, f in enumerate(y):
+                kw = {}
+                kw['name'] = f'f{i}'
+                if style[i] in ('-', None):
+                    kw['pen'] = 'b'
+                elif style[i] == '.':
+                    kw['pen'] = None
+                    kw['symbol'] = 'o'
+                elif style[i] == '.-':
+                    kw['pen'] = 'b'
+                    kw['symbol'] = 'o'
+                else:
+                    raise ValueError(f'Supported styles: ".", "-", ".-", got {style[i]}')
+                if isinstance(f, np.ndarray):
+                    if self.x[i] is None:
+                        self.static_plots.append(self.canvas.plot(np.arange(len(f)), f, **kw))
+                    else:
+                        self.static_plots.append(self.canvas.plot(self.x[i], f, **kw))
+                else:
+                    self.plots.append(self.canvas.plot([], [], **kw))
+                    self.funcs.append(f)
+                    self.funcs_x.append(self.x[i])
+                    for k, v in inspect.signature(f).parameters.items():
+                        default_args[k] = None if v.default is inspect._empty else v.default
             self.layout.addWidget(self.canvas)
 
             self.arg_names = []
             self.grid_row = 0
             self.grid = QGridLayout()
-            if isinstance(x, (tuple, list)):
-                assert len(x) == self.nfuncs, 'The number of x arrays should either be 1 or match the number of funcs'
-                assert isinstance(x[0], np.ndarray)
-                self.x = x
-            elif isinstance(x, np.ndarray) or x is None:
-                self.x = [x] * self.nfuncs
-            else:
-                raise ValueError('First argument x must either be a numpy array or a list of numpy arrays')
+
             self.func_kw = [list(inspect.signature(f).parameters) for f in self.funcs]
+
+            processed = set()
+            # parse function arguments
             for k, v in kwargs.items():
                 if isinstance(v, (tuple, list)):
-                    import pdb; pdb.set_trace()
-                    default = inspect.signature(f).parameters[k].default
-                    if default is inspect._empty:
-                        default = None
+                    default = default_args[k]
                     if len(v) == 2:
                         self.add_param(k, vmin=v[0], vmax=v[1], v=default)
                     elif len(v) == 3:
@@ -107,6 +149,11 @@ class SimpleWindow(QWidget):
                     else:
                         raise ValueError('tuple/list is expected to be 2 or 3 items long')
                 elif isinstance(v, (int, float)):
+                    self.add_param(k, v=v)
+                processed.add(k)
+
+            for k, v in default_args.items():
+                if v is not None and k not in processed:
                     self.add_param(k, v=v)
             self.layout.addLayout(self.grid)
             self.update()
@@ -145,25 +192,25 @@ class SimpleWindow(QWidget):
                 else:
                     current[k] = value
             for i, p in enumerate(self.plots):
-                if self.x[i] is None:
+                if self.funcs_x[i] is None:
                     kw = {k: current[k] for k in self.func_kw[i]}
                     y = self.funcs[i](**kw)
                     p.setData({'y': y})
                 else:
                     kw = {k: current[k] for k in self.func_kw[i] if k != 'x'}
-                    y = self.funcs[i](self.x[i], **kw)
-                    p.setData({'x': self.x[i], 'y': y})
+                    y = self.funcs[i](self.funcs_x[i], **kw)
+                    p.setData({'x': self.funcs_x[i], 'y': y})
         except:
             print_exc()
         
-def interact(*args, **kwargs):
+def iplot(*args, **kwargs):
     sw = SimpleWindow(*args, **kwargs)
     sw.show()
     return sw
 
-def test_interact():
+def test_iplot():
     def f(x, a, b):
         return np.exp(-a/100.*x) * np.sin(b*x)
 
-    interact(f, a=(1, 100, 1), b=(1, 10, 1))
+    iplot(f, a=(1, 100, 1), b=(1, 10, 1))
 
